165d8ee5c19c301f052bf334a00832199ac040e1
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-15 18:59:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,6 @@
 # don't forget to add: require 'pry'
 require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -11,7 +12,7 @@ end
 
 def engage
   date = generate_star_date
-  puts state_log(star_date)
+  puts state_log(date)
 end
 
 engage
